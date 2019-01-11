83f0f4bcf923c3063dd31ff9047ba77118346013
prestonedwardsjr <pres@imdiversity.com>
2019-01-11 10:55:41 -0600

    add index and js files

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-var timer =20;
+var timer = 20;
 var intervalId;
-var correct= 0;
-var incorrect=0;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+// Questions in Array 
 var questions = [{
-  question: " What year did The Saints win the SuperBowl?",
-  answerList: [1972,1989,2006,2009, ],
+  question: "What year did The Saints win the SuperBowl?",
+  answerList: [1972,1989,2006,2010,],
   answer: 3
-},];
-  $("#start").on("click",function(){
-$(this).hide();
+},
+];
+// Start Button
+$("#start").on("click", function() {
+  // Hide Start button
+  $(this).hide();
 // Display initial time countdown
-$("#time").html("<h2>Time Remaining: 120 Seconds</h2>" + "<br>");
-// Start Timer Countdown
-run();
+  $("#time").html("<h2>Time Remaining: 20 Seconds</h2>" + "<br>");
+// Start timer
+  run();
 // Question 1
-    $("#question0").html
+  $("#question0").html
   ("<h3>" + questions[0].question + "</h3>");
   $("#answer1").html
   ("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[0].answerList[0] + "</label>"
@@ -23,67 +27,72 @@ run();
    + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[0].answerList[3] + "</label><br><br>");
 // Done button
 
-$("#submit").html("<button id='done' type='button' class='btn btn-primary'>Done</button>");
-    $('#done').on("click", function() {
-    
+  $("#submit").html("<button id='done' type='button' class='btn btn-primary'>Done</button>");
 
-    // Keeping track of score based on correct, and incorrect, 
+  // Click event runs keepingScore() and displayResults() when user clicks Done button
+  $('#done').on("click", function() {
+ // Keeping track of score based on correct, incorrect, and unanswered
     keepingScore();
-
     // Display 
     displayResults();
-
   });
 });
-
 // Timer countdown function
 function run() {
-
-clearInterval(intervalId);
-intervalId = setInterval(decrement, 1000);
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
 }
-function decrement() {
-
 //  Decrease timer by one.
-  
-timer--;
+function decrement() { 
+  timer--;
+//  Show countdown in the #time tag
+  $("#time").html("<h2>Time Remaining: " + timer+ " Seconds</h2>" + "<br>");
+// once timer runs out of time
+  if (timer === 0) {
 
-//  Show the timer in the #time tag
-$("#time").html("<h2>Time Remaining: " + timer+ " Seconds</h2>" + "<br>");
-
-if (timer === 0) {
-
-  // Run stop function to stop timer countdown
-  stop();
+    stop();
     keepingScore();
     displayResults();
+  }
 }
-}
+// Run stop function to stop timer countdown
 function stop() {
-
 //  Clears intervalId
-clearInterval(intervalId);
-}  
+  clearInterval(intervalId);
+} 
 function displayResults() {
- 
   
- $("#time").hide();
- $("#question0").hide();
- $("#answer1").hide();
- $("#submit").hide();
- $("#resultsMessage").html("<h3>Results</h3>");
+  $("#time").hide();
+  $("#question0").hide();
+  $("#answer1").hide();
+  $("#question1").hide();
+  $("#submit").hide();
+
+  $("#resultsMessage").html("<h3>Results</h3>");
   $("#correct").html("Correct Answers: " + correctAnswers);
   $("#incorrect").html("Incorrect Answers: " + incorrectAnswers);
-  $("#unanswered").html("Unanswered: " + unanswered);
-}
+  
+}              
+//capture answers
+function keepingScore() {
+
+  var userAnswer1 = $("input[name='answer1']:checked").val();
+
+  if (userAnswer1 == questions[0].answer) {
+
+    correctAnswers++;
+  }
+  else {
+
+    incorrectAnswers++;
+  } 
+  }
 
 
 
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
